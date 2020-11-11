26713f8327221093c13f1b63e9bc4c228cda9fea
sboris <smrnvbrs@gmail.com>
2020-11-11 16:38:22 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QuickSort />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import QuickSort from "@/components/QuickSort";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    QuickSort,
+  },
+};
 </script>
 
 <style>
